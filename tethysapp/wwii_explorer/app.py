865cdc1fcac12813abec8fdefaa09dc15a67d17b
shawncrawley <shawncrawley@gmail.com>
2016-02-15 13:23:07 -0700

    New files added to GITHUB

---
+++ tethysapp/wwii_explorer/app.py
@@ -0,0 +1,31 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class WwiiExplorer(TethysAppBase):
+    """
+    Tethys app class for WWII Explorer.
+    """
+
+    name = 'WWII Explorer'
+    index = 'wwii_explorer:home'
+    icon = 'wwii_explorer/images/icon.gif'
+    package = 'wwii_explorer'
+    root_url = 'wwii-explorer'
+    color = '#34495e'
+    description = 'Place a brief description of your app here.'
+    enable_feedback = False
+    feedback_emails = []
+
+        
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (UrlMap(name='home',
+                           url='wwii-explorer',
+                           controller='wwii_explorer.controllers.home'),
+        )
+
+        return url_maps
