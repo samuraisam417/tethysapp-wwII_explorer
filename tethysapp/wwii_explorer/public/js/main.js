5466e3f47747785dc417ef8295d7de48c55bb564
shawncrawley <shawncrawley@gmail.com>
2016-02-23 13:03:13 -0700

    added Germany KML file; plotted KML on map; resized map

--- tethysapp/wwii_explorer/public/js/main.js
+++ tethysapp/wwii_explorer/public/js/main.js
@@ -10,3 +10,11 @@ var map = new ol.Map({
       zoom: 4
     })
 });
+
+var kmlLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+          url: '/static/wwii_explorer/data/Germany.kml',
+          format: new ol.format.KML()
+        })
+      });
+map.addLayer(kmlLayer);
