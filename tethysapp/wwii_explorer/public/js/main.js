37f981a086ae4074cb39619ab722ad9be564dde7
shawncrawley <shawncrawley@gmail.com>
2016-03-01 08:40:27 -0700

    beginnings of hashchange code

--- tethysapp/wwii_explorer/public/js/main.js
+++ tethysapp/wwii_explorer/public/js/main.js
@@ -1,16 +1,28 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: 'sat'})
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([11.040, 48.339]),
-      zoom: 3
-    })
+$(function () {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.MapQuest({layer: 'sat'})
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([11.040, 48.339]),
+            zoom: 3
+        })
+    });
+
+    window.onhashchange = getPageData;
+    window.location.hash = '0';
+
 });
 
+var getPageData = function () {
+    pageIndex = window.location.hash;
+    $.ajax({
+        
+    })
+};
 
 
 //$.
@@ -19,11 +31,11 @@ var map = new ol.Map({
 //Create function that will load the map with the desired KML files
 
 var kmlLayer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          url: '/static/wwii_explorer/data/kml/Germany.kml',
-          format: new ol.format.KML()
-        })
-      });
+    source: new ol.source.Vector({
+        url: '/static/wwii_explorer/data/kml/Germany.kml',
+        format: new ol.format.KML()
+    })
+});
 map.addLayer(kmlLayer);
 
 //kmlLayer = new ol.layer.Vector({
